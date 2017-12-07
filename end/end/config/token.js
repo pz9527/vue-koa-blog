bee28db62b8875f95f494bd48b6a78d84457bb22
pz9527 <575676885@qq.com>
2017-12-07 12:52:22 +0800

    修改错误

--- end/end/config/token.js
+++
@@ -1,5 +0,0 @@
-let jwtconfig = {
-    secret: 'myjwtsecret',
-    exprisesIn: '1800s'
-}
-module.exports = jwtconfig
--- end/end/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('koa-router')()
-const user = require('../controller/user')
-router.prefix('/user')
-router.post('/login', user.login)
-router.post('/signup',user.signUp)
-router.get('/:id', user.getUserById)
-module.exports = router
